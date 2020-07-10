1a561f44fd1ff6cbcfcbb4868ef4d1678e6c8d64
karlmiche <karlymism@gmail.com>
2020-07-10 16:48:54 -0700

    hoping and dreaming of working app

---
+++ gcpSetup.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+require("dotenv").config();
+fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GCRED, err => {
+    if (err) console.log(err) 
+    console.log("Writing a file 💾")
+})
